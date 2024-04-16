88f8fc5d04968771e3798cea089bb83e9f2d2efd
NabilRaiyan <rraiyan77@gmail.com>
2024-04-16 07:39:27 +0600

    adding login

---
+++ views/login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+
+    <label for="">Email: </label>
+    <input type="email" name="email" id="">
+
+    <br>
+    <br>
+    
+    <label for="">Password: </label>
+    <input type="password" name="password" id="">
+    
+</body>
+</html>

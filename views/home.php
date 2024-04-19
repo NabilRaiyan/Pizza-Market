b82bb9cf1fedbe2a251089864358090b0cba1009
NabilRaiyan <rraiyan77@gmail.com>
2024-04-19 23:24:43 +0600

    adding left and right upper setion

--- views/home.php
+++ views/home.php
@@ -4,6 +4,16 @@
 
 <?php include "header.php"; ?>
 
+<!-- left upper section  -->
+<div class="left-upper-section">
+    <h1>Good Food and Good Mood</h1>
+</div>
+
+<!-- right upper setion -->
+<div class="right-upper-section">
+    <h1>Piiza part</h1>
+</div>
+
 </body>
 </html>
 

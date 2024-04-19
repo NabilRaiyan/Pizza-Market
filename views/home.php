2f824542b318dbc07fda11a6d8c476db5ee3193e
NabilRaiyan <rraiyan77@gmail.com>
2024-04-20 01:40:06 +0600

    adding left and right upper section styles

--- views/home.php
+++ views/home.php
@@ -13,19 +13,21 @@
             <span class="title-word-color-black">& Good</span> <br> Moments
         </h1>
         <p class="upper-description">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            Facilisis leo vel fringilla est ullamcorper eget nulla facilisi. 
-            Mauris ultrices eros in cursus turpis massa tincidunt dui.
+            Eat the food you want to eat. don't need to come to us. Just make a call and we will be at your door with
+            your yummy food.Eat the food you want to eat. don't need to come to us. Just make a call and we will be at your door with
+            your yummy food.
         </p>
-        <a class="view-menu-btn">View Menu</a>
-        <h6 class="txt-happy-customer">Our Happy Customers</h6>
-        <img class="customer-img" src="../images/people.png" alt="people face" srcset="">
+        
+        <a href="#" class="view-menu-btn">View Menu</a>
+        <h5 class="txt-happy-customer">Our Happy Customers</h5>
+
+   
+        
     </div>
 
     <!-- right upper setion -->
     <div class="right-upper-section">
-        <h1>Piiza part</h1>
+        <h1 class="">Piiza part</h1>
     </div>
 </div>
 

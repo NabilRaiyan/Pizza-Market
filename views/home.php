5b3931338f83a7da310c26ad56765bdfe6fcb764
NabilRaiyan <rraiyan77@gmail.com>
2024-04-20 02:20:09 +0600

    adding left and right upper section styles

--- views/home.php
+++ views/home.php
@@ -11,7 +11,7 @@
         <h1 class="upper-title">
             Good <span class="title-word-color-black">Food</span> <br>
             <span class="title-word-color-black">& Good</span> <br> Moments
-        </h1>
+        </h1> 
         <p class="upper-description">
             Eat the food you want to eat. don't need to come to us. Just make a call and we will be at your door with
             your yummy food.Eat the food you want to eat. don't need to come to us. Just make a call and we will be at your door with
@@ -20,8 +20,8 @@
         
         <a href="#" class="view-menu-btn">View Menu</a>
         <h5 class="txt-happy-customer">Our Happy Customers</h5>
-
-   
+        <img class="customer-img" src="../images/people.png" alt="customer img" srcset="">
+        <h6 class="review-text">+12.5k Review</h6>
         
     </div>
 

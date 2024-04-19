01d725e0f3d6f63ae97fab16cf46cdcec28b0113
NabilRaiyan <rraiyan77@gmail.com>
2024-04-19 22:52:33 +0600

    adding navbar

--- views/header.php
+++ views/header.php
@@ -3,15 +3,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pizza Market</title>
     <link rel="stylesheet" href="../styles/styles.css">
+
+    <!-- google font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet">
 </head>
-<nav class="navbar">
-    <div class="nav-menu-div">
-        <a href="#">Pizza Mart</a>
-        <ul class="nav-menu-list">
-            <li class="nav-menu"><a href="#">Menu</a></li>
-            <li class="nav-menu"><a href="#">About Us</a></li>
-            <li class="nav-menu"><a href="#">Contact Us</a></li>
-        </ul>
-    </div>
-</nav>
+<div class="upper-section">
+    <nav class="navbar">
+        <div class="nav-menu-div">
+            <a href="#" class="logo">Pizza Mart</a>
+            <ul class="nav-menu-list">
+                <li class="nav-menu"><a href="#" class="menu-link">Home</a></li>
+                <li class="nav-menu"><a href="#" class="menu-link">Menu</a></li>
+                <li class="nav-menu"><a href="#" class="menu-link">About Us</a></li>
+                <li class="nav-menu"><a href="#" class="menu-link">Contact Us</a></li>
+            </ul>
+        </div>
+    </nav>
+</div>
+
 <body>

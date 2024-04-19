af0643a4b3e8aad27038648fbcb674c1a2a26b67
NabilRaiyan <rraiyan77@gmail.com>
2024-04-19 22:56:43 +0600

    adding navbar

--- views/header.php
+++ views/header.php
@@ -7,12 +7,15 @@
     <!-- google font -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=Work+Sans:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+
+    
+    
 </head>
 <div class="upper-section">
     <nav class="navbar">
         <div class="nav-menu-div">
-            <a href="#" class="logo">Pizza Mart</a>
+            <a href="#" class="logo">pizza mart</a>
             <ul class="nav-menu-list">
                 <li class="nav-menu"><a href="#" class="menu-link">Home</a></li>
                 <li class="nav-menu"><a href="#" class="menu-link">Menu</a></li>

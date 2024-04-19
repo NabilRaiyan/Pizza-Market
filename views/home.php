0b6d4b62e1c7e648c17725947c13ef6678a7bc28
NabilRaiyan <rraiyan77@gmail.com>
2024-04-20 00:45:46 +0600

    adding left and right upper section styles

--- views/home.php
+++ views/home.php
@@ -9,12 +9,14 @@
 
     <div class="left-upper-section">
         <h1 class="upper-title">
-            Good <span class="title-word-color-black">Food</span> 
-            <span class="title-word-color-black">& Good</span> Moments
+            Good <span class="title-word-color-black">Food</span> <br>
+            <span class="title-word-color-black">& Good</span> <br> Moments
         </h1>
         <p class="upper-description">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-            Facilisis leo vel fringilla est ullamcorper eget nulla facilisi. Mauris ultrices eros in cursus turpis massa tincidunt dui.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Facilisis leo vel fringilla est ullamcorper eget nulla facilisi. 
+            Mauris ultrices eros in cursus turpis massa tincidunt dui.
         </p>
         <a class="view-menu-btn">View Menu</a>
         <h6 class="txt-happy-customer">Our Happy Customers</h6>

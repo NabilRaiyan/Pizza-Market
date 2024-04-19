ac19f23482d3d67f604fd6413eef6d74adabe727
NabilRaiyan <rraiyan77@gmail.com>
2024-04-19 23:17:09 +0600

    adding navbar

--- views/header.php
+++ views/header.php
@@ -7,9 +7,9 @@
     <!-- google font -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=Work+Sans:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=Work+Sans:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-    
     
 </head>
 <div class="upper-section">
@@ -21,6 +21,7 @@
                 <li class="nav-menu"><a href="#" class="menu-link">Menu</a></li>
                 <li class="nav-menu"><a href="#" class="menu-link">About Us</a></li>
                 <li class="nav-menu"><a href="#" class="menu-link">Contact Us</a></li>
+                <li class="nav-menu cart-menu"><a href="#" class="cart-link"><i class="cart fa-solid fa-cart-shopping data-fa-transform="right-6"></i></a></li>
             </ul>
         </div>
     </nav>

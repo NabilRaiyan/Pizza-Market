4274615ed998e6510eb22cb8243bc94ab7fbad5d
NabilRaiyan <rraiyan77@gmail.com>
2024-04-22 22:22:09 +0600

    adding cart

--- views/popularItemCart.php
+++ views/popularItemCart.php
@@ -7,9 +7,13 @@
         <span class="rating fa fa-star checked"></span>
         <span class="rating fa fa-star"></span>
         <span class="rating fa fa-star"></span>
+        <hr class="card-hr">
             <h2 class="cart-title">
                 Peparoni Pizza
             </h2>
+            <h5 class="cart-subtitle">
+                
+            </h5>
 
         </div>
     </div>
